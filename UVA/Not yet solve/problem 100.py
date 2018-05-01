36146f7b4ed90c26b996c9ee7dbd79e2dfb6e949
Faiz Kwan <faizikhwantop@gmail.com>
2018-05-01 20:16:03 +0800

    update p100

--- UVA/Not yet solve/problem 100.py
+++ UVA/Not yet solve/problem 100.py
@@ -1,24 +1,30 @@
 #Max iterations between two numbers (num1,num2)
-# UNFINISH
-def counting(num):
-    count = 0;
-    while True:
-        #print(int(num), end=' ')
-        count = count + 1
-        if num == 1:
-            break
-        if (num%2) == 0:
-            num = num / 2
-        elif num%2 == 1:
-            num = 3 * num + 1
-    return count
+#TLE
 
-num1,num2 = input().split()
-while num1!= '' and num2!='':
-    max = counting(int(num1))
-    for i in range(int(num1), int(num2)+1):
-        total = counting(i)
-        if total> max:
-            max = total
-    print(int(num1), int(num2), max)
-    num1, num2 = input().split()
+def counting(num1,num2):
+    res = 0;
+    
+    if num1 > num2:
+        temp = num1
+        num1 = num2
+        num2 = temp
+        
+    for i in range(num1,num2+1):
+        no = i
+        cl = 0
+        while no != 1:        
+            cl += 1
+            if (no%2) == 0:
+                no = no / 2
+            elif no%2 == 1:
+                no = 3 * no + 1
+        res = max(res, cl+1)
+    return res
+
+
+while True:
+    num1,num2 = input().split()
+    if num1 == ''and num2 == '':
+        break
+    else:
+        print('{} {} {}'.format(num1, num2, counting(int(num1),int(num2))))
